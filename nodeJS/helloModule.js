e9140a9853d834464c2da7ba7fbfc4106102402f
ringo <2479733418@qq.com>
2021-08-16 15:03:23 +0800

    8.16-前端学习

---
+++ nodeJS/helloModule.js
@@ -0,0 +1,13 @@
+// exports.name = '孙大圣'
+// exports.age = 18
+// exports.sayNames = function () {
+//     console.log(this.name)
+// }
+
+module.exports = {
+    name:'猪八戒',
+    age:28,
+    sayNames:function () {
+            console.log(this.name)
+        }
+}
